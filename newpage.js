e68996c1cbef42cc709857a4755cf40f39b1fd9e
Ibrahim Saberi <georgeipsum@gmail.com>
2019-11-01 18:47:15 -0400

    added basic growth tracking

--- newpage.js
+++ newpage.js
@@ -1,5 +1,20 @@
+let growth = 0;
+let cancelGrowth;
+
+chrome.storage.sync.get(['growth'], function(result) {
+  if(!result) {
+    chrome.storage.sync.set({growth: 0}, () => {
+      console.log("Let's begin our growth.");
+      growth = 0;
+    })
+  }
+
+  growth = new Number(result.key);
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   setTimerElement();
+  const removeListener = setCanvas();
 
   const timer = setInterval(setTimerElement ,1000);
 
@@ -7,18 +22,77 @@ document.addEventListener("DOMContentLoaded", () => {
     const event = isEventSupported('beforeunload') ? "beforeunload" : "unload";
     window.addEventListener(event, () => {
       clearInterval(timer);
+      removeListener();
     });
   }
 
   setTimeout(() => {
     determineBodyBg(new Date().getHours());
   }, 150);
+
+  setTimeout(animateIn ,450);
+
+  const clear = document.getElementById("clear");
+  if(clear) {
+    clear.addEventListener("click", () => {
+      growth = 0;
+      chrome.storage.sync.set({growth}, () => {
+        clearInterval(cancelGrowth);
+        grow(growth);
+        console.log("Growth reset");
+      });
+    });
+  }
 });
+
+const animateIn = () => {
+  const grass = document.getElementById("grass");
+  const dirt = document.getElementById("dirt");
+  const canvas = document.getElementById("tree");
+
+  grass.style.height = '50px';
+  dirt.style.height = '100px';
+  canvas.style.opacity = 1;
+
+  grow(growth);
+}
+
+const grow = (initialGrowth) => {
+  let growth = new Number(initialGrowth);
+  cancelGrowth = setInterval(() => {
+    growth += 1;
+    chrome.storage.sync.set({growth}, () => {
+      console.log(`Growth at level ${growth}!`);
+    });
+  }, 5000);
+}
+
+const setCanvas = () => {
+  const canvas = document.getElementById("tree");
+  if(canvas) {
+    canvas.width = window.innerWidth*.80;
+    canvas.height = window.innerHeight-250;
+  }
+
+  const resizeListener = (event) => {
+    const canvas = document.getElementById("tree");
+    if(canvas) {
+      canvas.width = window.innerWidth*.80;
+      canvas.height = window.innerHeight-250;
+    }
+  };
+
+  window.addEventListener("resize", resizeListener);
+
+  return () => {
+    window.removeEventListener("resize", resizeListener);
+  }
+}
 
 const setTimerElement = () => {
   const date = new Date();
   const hour = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
-  const minutes = date.getMinutes();
+  const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
   const dateString = hour + ':' + minutes;
   const formattedDateString = date.toISOString();
 
